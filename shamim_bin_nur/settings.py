b1c8a7a7e575c48c7e2853fbbb15acddff7b8bcf
icecream <zanjarwhite@gmail.com>
2019-03-29 00:39:47 +0600

    upload with error

--- shamim_bin_nur/settings.py
+++ shamim_bin_nur/settings.py
@@ -28,6 +28,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rangefilter',
+    'django_mysql',
     #created app
     'apps',
 ]
@@ -65,11 +66,14 @@ WSGI_APPLICATION = 'shamim_bin_nur.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME':'database',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
 
